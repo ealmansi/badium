56d597d72e362656ba23aae1ffdfb7c29ea0e670
Emilio Almansi <ealmansi@gmail.com>
2019-05-16 20:07:02 -0300

    Add WIP implementation and tests.

--- contracts/Badium.sol
+++ contracts/Badium.sol
@@ -1,5 +1,289 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Badium {
-  constructor () public {}
+  /**
+   *
+   * Constants.
+   *
+   */
+
+  uint256 constant UINT256_MAX = ~uint256(0);
+  string constant TOKEN_NAME = "Badium";
+  string constant TOKEN_SYMBOL = "BAD";
+  uint256 constant INITIAL_SUPPLY = 10000000;
+  uint256 constant EXCHANGE_RATE = 0.01 ether;
+
+  /**
+   *
+   * Events.
+   *
+   */
+
+  event Transfer (address indexed from, address indexed to, uint256 amount);
+  event Approval (address indexed holder, address indexed spender, uint256 amount);
+
+  /**
+   *
+   * Internal state.
+   *
+   */
+
+  address payable private _owner;
+
+  string private _name;
+
+  string private _symbol;
+
+  uint256 private _burnCount;
+
+  mapping (
+    uint256 => uint256
+  ) private _totalSupply;
+
+  mapping (
+    uint256 => mapping (
+      address => uint256
+    )
+  ) private _balances;
+
+  mapping (
+    uint256 => mapping (
+      address => mapping (
+        address => uint256
+      )
+    )
+  ) private _allowances;
+
+  mapping (
+    address => bool
+  ) private _receivers;
+
+  /**
+   *
+   * Modifiers.
+   *
+   */
+
+  /**
+   *
+   */
+  modifier onlyOwner() {
+    address _sender = msg.sender;
+    require(_sender == _owner, "Badium: Unauthorized.");
+    _;
+  }
+
+  /**
+   *
+   * Implementation.
+   *
+   */
+
+  /**
+   *
+   */
+  constructor () public {
+    _owner = msg.sender;
+    _name = TOKEN_NAME;
+    _symbol = TOKEN_SYMBOL;
+    _totalSupply[_burnCount] = INITIAL_SUPPLY;
+    _balances[_burnCount][_owner] = _totalSupply[_burnCount];
+  }
+
+  /**
+   *
+   * Optional ERC20 features.
+   *
+   */
+
+  /**
+   * @return The token's name.
+   */
+  function name () public view returns (string memory) {
+    return _name;
+  }
+
+  /**
+   * @return The token's symbol.
+   */
+  function symbol () public view returns (string memory) {
+    return _symbol;
+  }
+
+  /**
+   *
+   * Mandatory ERC20 features.
+   *
+   */
+
+  /**
+   * @return The token's current total supply.
+   */
+  function totalSupply () public view returns (uint256) {
+    return _totalSupply[_burnCount];
+  }
+
+  /**
+   * @param holder Token holder's address.
+   * @return The balance of the given address.
+   */
+  function balanceOf (address holder) public view returns (uint256) {
+    return _balances[_burnCount][holder];
+  }
+
+  /**
+   * @dev Transfer.
+   * @return True on success, else false.
+   */
+  function transfer (address to, uint256 amount) public returns (bool) {
+    require(_receivers[to], "Badium: Invalid receiver.");
+
+    address _sender = msg.sender;
+    return transferFrom(_sender, to, amount);
+  }
+
+  /**
+   * @dev TransferFrom.
+   * @return True on success, else false.
+   */
+  function transferFrom (address from, address to, uint256 amount) public returns (bool) {
+    require(_receivers[to], "Badium: Invalid receiver.");
+
+    address _sender = msg.sender;
+    if (_sender != from && _sender != _owner) {
+      require(amount <= _allowances[_burnCount][from][_sender], "Badium: Insufficient allowance.");
+      _allowances[_burnCount][from][_sender] -= amount;
+    }
+
+    require(amount <= _balances[_burnCount][from], "Badium: Insufficient balance.");
+    _balances[_burnCount][from] -= amount;
+
+    require(amount <= UINT256_MAX - _balances[_burnCount][to], "Badium: Balance limit exceeded.");
+    _balances[_burnCount][to] += amount;
+
+    emit Transfer(from, to, amount);
+    return true;
+  }
+
+  /**
+   * @dev Approve.
+   * @return True on success, else false.
+   */
+  function approve (address spender, uint256 amount) public returns (bool) {
+    address _sender = msg.sender;
+    _allowances[_burnCount][_sender][spender] = amount;
+
+    emit Approval(_sender, spender, amount);
+    return true;
+  }
+
+  /**
+   *
+   */
+  function allowance (address holder, address spender) public view returns (uint256) {
+    return _allowances[_burnCount][holder][spender];
+  }
+
+  /**
+   *
+   * Custom Badium features.
+   *
+   */
+
+  /**
+   * @dev Purchases the given amount of BAD tokens by exchanging them for the Ether received in the
+   * incoming transaction at a rate of 0.01 ETH per BAD token. The purchased tokens will be assigned
+   * to the sender's address.
+   * @param amount The amount of BAD tokens to buy.
+   * @return True on success, else false.
+   */
+  function buy (uint256 amount) public payable returns (bool) {
+    uint256 value = msg.value;
+    require(
+      value > 0 &&
+      value % EXCHANGE_RATE == 0 &&
+      value / EXCHANGE_RATE == amount,
+      "Badium: Invalid transaction value."
+    );
+
+    require(amount <= _balances[_burnCount][_owner], "Badium: Insufficient available supply.");
+    _balances[_burnCount][_owner] -= amount;
+
+    address _sender = msg.sender;
+    require(
+      amount <= UINT256_MAX - _balances[_burnCount][_sender],
+      "Badium: Balance limit exceeded."
+    );
+    _balances[_burnCount][_sender] += amount;
+
+    return true;
+  }
+
+  /**
+   * @dev Withdraws the specified value in Wei by transfering funds to the owners' address.
+   * @param value The amount of Wei to withdraw.
+   * @return True on success, else false.
+   */
+  function withdraw (uint256 value) public onlyOwner returns (bool) {
+    uint256 _balance = address(this).balance;
+    require(value <= _balance, "Badium: Insufficient balance.");
+    _owner.transfer(value);
+
+    return true;
+  }
+
+  /**
+   * @dev Erases all issued tokens from existence.
+   * @return True on success, else false.
+   */
+  function burn () public onlyOwner returns (bool) {
+    require(1 <= UINT256_MAX - _burnCount, "Badium: Burn count limit exceeded.");
+    _burnCount += 1;
+
+    return true;
+  }
+
+  /**
+   * @dev Increases the total supply by the specified amount of tokens. The created tokens are
+   * assigned to the owner's account.
+   * @param amount The amount of tokens to be minted.
+   * @return True on success, else false.
+   */
+  function mint (uint256 amount) public onlyOwner returns (bool) {
+    require(
+      amount <= UINT256_MAX - _totalSupply[_burnCount],
+      "Badium: Total supply limit exceeded."
+    );
+    _totalSupply[_burnCount] += amount;
+
+    require(
+      amount <= UINT256_MAX - _balances[_burnCount][_owner],
+      "Badium: Balance limit exceeded."
+    );
+    _balances[_burnCount][_owner] += amount;
+
+    return true;
+  }
+
+  /**
+   * @dev Adds the given address to the list of approved receivers.
+   * @param receiver The receiver's address.
+   * @return True on success, else false.
+   */
+  function addReceiver (address receiver) public onlyOwner returns (bool) {
+    _receivers[receiver] = true;
+
+    return true;
+  }
+
+  /**
+   * @dev Removes the given address from the list of approved receivers.
+   * @param receiver The receiver's address.
+   * @return True on success, else false.
+   */
+  function removeReceiver (address receiver) public onlyOwner returns (bool) {
+    _receivers[receiver] = false;
+
+    return true;
+  }
 }
--- test/badium.js
+++ test/badium.js
@@ -1,6 +1,8 @@
 const Badium = artifacts.require('Badium')
 
 contract('Badium', () => {
+  const UINT256_MAX = web3.utils.toBN(web3.utils.toTwosComplement(-1))
+
   let accounts = null
   let instance = null
 
@@ -14,11 +16,292 @@ contract('Badium', () => {
     instance = null
   })
 
-  it('should load accounts successfully', async () => {
-    assert.ok(accounts)
+  it('should be called Badium', async () => {
+    let name = await instance.name()
+    assert.strictEqual(name, 'Badium')
   })
 
-  it('should create a contract instance successfully', async () => {
-    assert.ok(instance)
+  it('should have symbol BAD', async () => {
+    let name = await instance.symbol()
+    assert.strictEqual(name, 'BAD')
+  })
+
+  it('should have an initial total supply of 10M', async () => {
+    let totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+  })
+
+  it('should support querying an address\' balance', async () => {
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 10000000)
+    assert.strictEqual(balance1.toNumber(), 0)
+  })
+
+  it('should allow the contract owner to approve token receivers', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+  })
+
+  it('should allow the contract owner to remove approval from token receivers', async () => {
+    assert.ok(await instance.removeReceiver(accounts[1]))
+  })
+
+  it('should allow token holders to transfer their tokens to approved receivers', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    assert.ok(await instance.transfer(accounts[1], 1))
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 9999999)
+    assert.strictEqual(balance1.toNumber(), 1)
+  })
+
+  it('should prevent token holders from transfering to non-approved receivers (1)', async () => {
+    await assertThrows(
+      async () => {
+        await instance.transfer(accounts[1], 1)
+      },
+      'Badium: Invalid receiver.'
+    )
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 10000000)
+    assert.strictEqual(balance1.toNumber(), 0)
+  })
+
+  it('should prevent token holders from transfering to non-approved receivers (2)', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    assert.ok(await instance.removeReceiver(accounts[1]))
+    await assertThrows(
+      async () => {
+        await instance.transfer(accounts[1], 1)
+      },
+      'Badium: Invalid receiver.'
+    )
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 10000000)
+    assert.strictEqual(balance1.toNumber(), 0)
+  })
+
+  it('should support arbitrary transfers by the contract owner', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    assert.ok(await instance.addReceiver(accounts[2]))
+    assert.ok(await instance.transfer(accounts[1], 2))
+    assert.ok(await instance.transferFrom(accounts[1], accounts[2], 1))
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    let balance2 = await instance.balanceOf(accounts[2])
+    assert.strictEqual(balance0.toNumber(), 9999998)
+    assert.strictEqual(balance1.toNumber(), 1)
+    assert.strictEqual(balance2.toNumber(), 1)
+  })
+
+  it('should fail if trying to transfer more than the available funds', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    await assertThrows(
+      async () => {
+        await instance.transfer(accounts[1], 10000001)
+      },
+      'Badium: Insufficient balance.'
+    )
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 10000000)
+    assert.strictEqual(balance1.toNumber(), 0)
+  })
+
+  it('should allow token holders to set an allowance to a third-party', async () => {
+    assert.ok(await instance.approve(accounts[2], 1, { from: accounts[1] }))
+  })
+
+  it('should support querying a given third-party\'s allowance', async () => {
+    let allowance = await instance.allowance(accounts[0], accounts[1])
+    assert.strictEqual(allowance.toNumber(), 0)
+  })
+
+  it('should allow third-parties to do a transfer on behalf of a token holder', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    assert.ok(await instance.addReceiver(accounts[2]))
+    assert.ok(await instance.transfer(accounts[1], 2))
+    assert.ok(await instance.approve(accounts[2], 1, { from: accounts[1] }))
+    assert.ok(await instance.transferFrom(accounts[1], accounts[2], 1, { from: accounts[2] }))
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    let balance2 = await instance.balanceOf(accounts[2])
+    assert.strictEqual(balance0.toNumber(), 9999998)
+    assert.strictEqual(balance1.toNumber(), 1)
+    assert.strictEqual(balance2.toNumber(), 1)
+  })
+
+  it('should prevent third-parties from spending more than their allowance', async () => {
+    assert.ok(await instance.addReceiver(accounts[1]))
+    assert.ok(await instance.approve(accounts[1], 2))
+    await assertThrows(
+      async () => {
+        await instance.transferFrom(accounts[0], accounts[1], 3, { from: accounts[1] })
+      },
+      'Badium: Insufficient allowance.'
+    )
+    assert.ok(await instance.transferFrom(accounts[0], accounts[1], 2, { from: accounts[1] }))
+    await assertThrows(
+      async () => {
+        await instance.transferFrom(accounts[0], accounts[1], 1, { from: accounts[1] })
+      },
+      'Badium: Insufficient allowance.'
+    )
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    assert.strictEqual(balance0.toNumber(), 9999998)
+    assert.strictEqual(balance1.toNumber(), 2)
+  })
+
+  it('should support buying tokens at a cost of 0.01 ETH per BAD', async () => {
+    assert.ok(await instance.buy(1, { from: accounts[1], value: web3.utils.toWei('0.01') }))
+    assert.ok(await instance.buy(10, { from: accounts[2], value: web3.utils.toWei('0.1') }))
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    let balance2 = await instance.balanceOf(accounts[2])
+    assert.strictEqual(balance0.toNumber(), 9999989)
+    assert.strictEqual(balance1.toNumber(), 1)
+    assert.strictEqual(balance2.toNumber(), 10)
+    let balance3 = await web3.eth.getBalance(instance.address)
+    assert.strictEqual(web3.utils.fromWei(balance3), '0.11')
+  })
+
+  it('should support burning all tokens by the contract owner (1)', async () => {
+    assert.ok(await instance.buy(1, { from: accounts[1], value: web3.utils.toWei('0.01') }))
+    assert.ok(await instance.buy(1, { from: accounts[2], value: web3.utils.toWei('0.01') }))
+    assert.ok(await instance.addReceiver(accounts[3]))
+    assert.ok(await instance.addReceiver(accounts[4]))
+    assert.ok(await instance.transferFrom(accounts[0], accounts[3], 1))
+    assert.ok(await instance.transferFrom(accounts[0], accounts[4], 1))
+    let totalSupply = await instance.totalSupply()
+    let balance0 = await instance.balanceOf(accounts[0])
+    let balance1 = await instance.balanceOf(accounts[1])
+    let balance2 = await instance.balanceOf(accounts[2])
+    let balance3 = await instance.balanceOf(accounts[3])
+    let balance4 = await instance.balanceOf(accounts[4])
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+    assert.strictEqual(balance0.toNumber(), 9999996)
+    assert.strictEqual(balance1.toNumber(), 1)
+    assert.strictEqual(balance2.toNumber(), 1)
+    assert.strictEqual(balance3.toNumber(), 1)
+    assert.strictEqual(balance4.toNumber(), 1)
+    assert.ok(await instance.burn())
+    totalSupply = await instance.totalSupply()
+    balance0 = await instance.balanceOf(accounts[0])
+    balance1 = await instance.balanceOf(accounts[1])
+    balance2 = await instance.balanceOf(accounts[2])
+    balance3 = await instance.balanceOf(accounts[3])
+    balance4 = await instance.balanceOf(accounts[4])
+    assert.strictEqual(totalSupply.toNumber(), 0)
+    assert.strictEqual(balance0.toNumber(), 0)
+    assert.strictEqual(balance1.toNumber(), 0)
+    assert.strictEqual(balance2.toNumber(), 0)
+    assert.strictEqual(balance3.toNumber(), 0)
+    assert.strictEqual(balance4.toNumber(), 0)
+  })
+
+  it('should support burning all tokens by the contract owner (2)', async () => {
+    let totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+    assert.ok(await instance.burn())
+    totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 0)
+    await instance.mint(100)
+    totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 100)
+    assert.ok(await instance.burn())
+    totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 0)
+  })
+
+  it('should support minting an arbitrary amount of tokens', async () => {
+    let totalSupply = await instance.totalSupply()
+    let balance = await instance.balanceOf(accounts[0])
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+    assert.strictEqual(balance.toNumber(), 10000000)
+    assert.ok(await instance.mint(1))
+    assert.ok(await instance.mint(10))
+    assert.ok(await instance.mint(100))
+    assert.ok(await instance.mint(1000))
+    assert.ok(await instance.mint(10000))
+    totalSupply = await instance.totalSupply()
+    balance = await instance.balanceOf(accounts[0])
+    assert.strictEqual(totalSupply.toNumber(), 10011111)
+    assert.strictEqual(balance.toNumber(), 10011111)
+  })
+
+  it('should fail if trying to mint past the total supply limit', async () => {
+    let totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+    await assertThrows(
+      async () => {
+        await instance.mint(UINT256_MAX)
+      },
+      'Badium: Total supply limit exceeded.'
+    )
+    totalSupply = await instance.totalSupply()
+    assert.strictEqual(totalSupply.toNumber(), 10000000)
+  })
+
+  it('should allow the contract owner to withdraw Ether collected from purchases', async () => {
+    assert.ok(await instance.buy(100, { from: accounts[1], value: web3.utils.toWei('1') }))
+    assert.ok(await instance.buy(100, { from: accounts[2], value: web3.utils.toWei('1') }))
+    let balance1 = await web3.eth.getBalance(accounts[0])
+    let balance2 = await web3.eth.getBalance(instance.address)
+    assert.ok(await instance.withdraw(balance2))
+    let balance3 = await web3.eth.getBalance(accounts[0])
+    let balance4 = await web3.eth.getBalance(instance.address)
+    let difference = Number(
+      web3.utils.fromWei(
+        web3.utils.toBN(balance1).add(web3.utils.toBN(balance2)).sub(web3.utils.toBN(balance3))
+      )
+    )
+    assert.ok(difference < 1e-2)
+    assert.strictEqual(web3.utils.fromWei(balance4), '0')
+  })
+
+  it('should fail if a user tries to access owner-only features', async () => {
+    await assertThrows(
+      async () => {
+        await instance.withdraw(1, { from: accounts[1] })
+      },
+      'Badium: Unauthorized.'
+    )
+    await assertThrows(
+      async () => {
+        await instance.burn({ from: accounts[1] })
+      },
+      'Badium: Unauthorized.'
+    )
+    await assertThrows(
+      async () => {
+        await instance.mint(1, { from: accounts[1] })
+      },
+      'Badium: Unauthorized.'
+    )
+    await assertThrows(
+      async () => {
+        await instance.addReceiver(accounts[0], { from: accounts[1] })
+      },
+      'Badium: Unauthorized.'
+    )
+    await assertThrows(
+      async () => {
+        await instance.removeReceiver(accounts[0], { from: accounts[1] })
+      },
+      'Badium: Unauthorized.'
+    )
   })
 })
+
+async function assertThrows (asyncFunction, errorMessage) {
+  try {
+    await asyncFunction()
+  } catch (err) {
+    assert.ok(err.message.indexOf(errorMessage) !== -1)
+    return
+  }
+  assert.ok(false, 'Expected error to be thrown.')
+}
